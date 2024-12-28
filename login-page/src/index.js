53631bdafcb2ee6516c647c51593bf0310043647
Mihir <mnkatakdhond_b23@ce.vjti.ac.in>
2024-12-29 00:37:29 +0530

    Update index.js and .env with correct Auth0 configurations

--- login-page/src/index.js
+++ login-page/src/index.js
@@ -6,13 +6,16 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+// Get the redirect URI from the environment variable, or fallback to the default for local development
+const redirectUri = process.env.REACT_APP_AUTH0_REDIRECT_URI || `${window.location.origin}/auth`;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Auth0Provider
     domain="dev-1gc7saqkj363r3ix.us.auth0.com"
     clientId="bJH2HtlExxm3osS2WiB7mgndPxsh7gf8"
     authorizationParams={{
-      redirect_uri: `${window.location.origin}/auth`,
+      redirect_uri: redirectUri,
       prompt: 'select_account',
       screen_hint: 'signup',
       response_type: 'code',
